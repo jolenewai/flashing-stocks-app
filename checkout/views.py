a695514e1ba52da4a226b5bc6b0a42c7fbe14c85
jolenewai <jolene.wai@gmail.com>
2020-05-29 12:23:55 +0000

    remove code for the checkout bug

--- checkout/views.py
+++ checkout/views.py
@@ -42,7 +42,7 @@ def checkout(request):
     session = stripe.checkout.Session.create(
         payment_method_types=['card'],
         line_items=line_items,
-        success_url=domain + reverse(checkout_success, args={'session': id}),
+        success_url=domain + reverse(checkout_success),
         cancel_url=domain + reverse(checkout_cancelled),
     )
 
