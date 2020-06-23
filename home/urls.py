76ff76da97209a03f01c3b0abbc0f67124509e7b
jolenewai <jolene.wai@gmail.com>
2020-06-23 07:44:34 +0000

    added comments

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,4 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
@@ -7,5 +6,5 @@ urlpatterns = [
     path('search', views.search, name='search'),
     path('search_by_tag/<tag_id>', views.search_by_tag, name='search_by_tag'),
     path('terms_of_use', views.terms_of_use, name='terms_of_use'),
-    path('license_agreements', views.license_agreements, name='license_agreements'), 
+    path('license_agreements', views.license_agreements, name='license_agreements'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, reverse
-from django.db.models import Q, Count
-from photos.models import Photo, Category, Tag
+from django.db.models import Q
+from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.models import Group
-from customers.models import Customer, Favourite, Download
+from photos.models import Photo, Category, Tag
+from customers.models import Customer, Favourite
 
 
 def index(request):
@@ -11,7 +12,6 @@ def index(request):
     customer_group = Group.objects.get(name='customers')
 
     if request.user.is_authenticated:
-
         if photographer_group in request.user.groups.all():
             return redirect(reverse('photographer_view_profile'))
         elif customer_group in request.user.groups.all():
@@ -28,89 +28,123 @@ def search(request):
     categories = Category.objects.all()
     tags = Tag.objects.all().order_by('name')
 
+    # initialise keyword to empty string
     keyword = ''
+
+    # if GET parameter exists
     if request.GET:
+
+        # always true query
         queries = ~Q(pk__in=[])
 
+        # get keyword if keyword found in query string
         if 'keyword' in request.GET and request.GET['keyword']:
             keyword = request.GET['keyword']
-            queries = queries & (Q(caption__icontains=keyword) | Q(desc__icontains=keyword) | Q(tags__name=keyword))
+            # get photo object that contains keyword in
+            # caption, description, tag and category name
+            queries = queries & (
+                Q(caption__icontains=keyword) |
+                Q(desc__icontains=keyword) |
+                Q(tags__name__icontains=keyword) |
+                Q(category__name__icontains=keyword)
+                )
 
+        # if category is found in query string
         if 'category' in request.GET and request.GET['category']:
             category = request.GET['category']
-            queries = queries & Q(category__id=category) 
+            # filter result by specified category
+            queries = queries & Q(category__id=category)
 
-        photos = photos.filter(queries)
+        # filter unique result with combined queries above
+        photos = photos.filter(queries).distinct()
 
+        # if sortby is found in query string
         if 'sortby' in request.GET and request.GET['sortby']:
+
+            # get the field to sort
             sortby_field = request.GET['sortby']
-            
+
+            # if 'Most Recent' is chosen for sorting
             if sortby_field == 'mr':
+                # sort result by 'Date added' field
                 photos = photos.order_by('date_added')
+            # if 'Alphabetical Order' is chosen for sorting
             elif sortby_field == 'al':
+                # sort result by 'caption' field
                 photos = photos.order_by('caption')
-            # elif sortby_field == 'pp':
-            #     downloads = Download.objects.values('image__id').annotate(num_download=Count('image')).order_by('-num_download')
 
-            #     queries = ~Q(pk__in=[])
-
-            #     if 'keyword' in request.GET and request.GET['keyword']:
-            #         keyword = request.GET['keyword']
-            #         queries = queries & (
-            #             Q(image__caption__icontains=keyword) | Q(image__desc__icontains=keyword) | Q(image__tags__name=keyword)
-            #         )
-
-            #     if 'category' in request.GET and request.GET['category']:
-            #         category = request.GET['category']
-            #         queries = queries & Q(image__category__id=category) 
-
-            #     photos = downloads.filter(queries)
-    
+    # do a count for the final search results
     photos_count = photos.count()
+
+    # to determine if a photo has been favourited by the user
+    # variable initialised as empty array to avoid error in template
     favourited_photo = []
 
-    
-    if request.user.is_authenticated:
-        customer = Customer.objects.get(user=request.user)
-        favourites = Favourite.objects.filter(user=customer)
+    # if user is logged in and in customer group
+    if request.user.is_authenticated and request.user.groups.first() == 'customers':
 
-        for fav in favourites:
-            favourited_photo.append(
-                fav.image
-            )
+        try:
+            customer = Customer.objects.get(user=request.user)
+        except ObjectDoesNotExist:
+            customer = None
+
+        # if customer profile exists, get favourite for this user
+        if customer:
+            favourites = Favourite.objects.filter(user=customer)
+
+            # append photo to favourited_photo array if record found
+            for fav in favourites:
+                favourited_photo.append(
+                    fav.image
+                )
 
     return render(request, 'home/search.template.html', {
-        'photos': photos,
-        'categories': categories,
-        'tags': tags,
-        'keyword': keyword,
-        'photos_count': photos_count,
-        'favourited_photo': favourited_photo
-    })
+            'photos': photos,
+            'categories': categories,
+            'tags': tags,
+            'keyword': keyword,
+            'photos_count': photos_count,
+            'favourited_photo': favourited_photo
+        })
 
 
 def search_by_tag(request, tag_id):
     selected_tag = Tag.objects.get(id=tag_id)
+    # get all photo objects for filtering later
     photos = Photo.objects.all()
     categories = Category.objects.all()
     tags = Tag.objects.all()
 
+    # always true query
     queries = ~Q(pk__in=[])
 
+    # create query with selected tag
     queries = queries & Q(tags=selected_tag)
 
+    # filter result with query
     photos = photos.filter(queries)
+
+    # initialise empty array for user favourite photos
     favourited_photo = []
     photos_count = photos.count()
 
-    if request.user.is_authenticated:
-        customer = Customer.objects.get(user=request.user)
-        favourites = Favourite.objects.filter(user=customer)
+    # if user is logged in and in customer group
+    if request.user.is_authenticated and request.user.groups.first() == 'customers':
 
-        for fav in favourites:
-            favourited_photo.append(
-                fav.image
-            )
+        try:
+            customer = Customer.objects.get(user=request.user)
+        except ObjectDoesNotExist:
+            customer = None
+
+        # if customer profile exists, get favourite for this user
+        if customer:
+            favourites = Favourite.objects.filter(user=customer)
+
+            # append photo to favourited_photo array if record found
+            for fav in favourites:
+                favourited_photo.append(
+                    fav.image
+                )
 
     return render(request, 'home/search.template.html', {
         'photos': photos,
